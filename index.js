b28d9ebc7b851ab71d963e34a3e60a2896473c06
Cleilson <marcialwushu@gmail.com>
2019-09-19 20:44:26 -0300

    Update Port for 3000

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ io.on('connection', (socket) => {
     });
 });
 
-var port = process.env.PORT || 3001;
+var port = process.env.PORT || 3000;
 
 http.listen(port, function(){
     console.log('listening in http://localhost:' + port)
